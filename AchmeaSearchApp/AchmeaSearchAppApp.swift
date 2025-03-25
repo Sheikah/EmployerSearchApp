170eb947f393ce7982e2922342658bfdf8419c53
Daan Vermeulen <daan.vermeulen@xs4all.nl>
2025-03-25 16:37:56 +0100

    XCode project created.

---
+++ AchmeaSearchApp/AchmeaSearchAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  AchmeaSearchAppApp.swift
+//  AchmeaSearchApp
+//
+//  Created by Annette Straver on 25/03/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct AchmeaSearchAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ AchmeaSearchAppTests/AchmeaSearchAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  AchmeaSearchAppTests.swift
+//  AchmeaSearchAppTests
+//
+//  Created by Annette Straver on 25/03/2025.
+//
+
+import Testing
+
+struct AchmeaSearchAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
