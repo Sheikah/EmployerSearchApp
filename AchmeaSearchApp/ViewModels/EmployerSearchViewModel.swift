9bd5bc04fecd4e235c28c2d97813f8c88f65f3c7
Daan Vermeulen <daan.vermeulen@xs4all.nl>
2025-03-25 18:41:23 +0100

    Adding the view model and prepared it to call the web service.

---
+++ AchmeaSearchApp/ViewModels/EmployerSearchViewModel.swift
@@ -0,0 +1,52 @@
+//
+//  EmployerSearchViewModel.swift
+//  AchmeaSearchApp
+//
+//  Created by Annette Straver on 25/03/2025.
+//
+
+import SwiftData
+import Foundation
+import SwiftUICore
+
+@Observable
+class EmployerSearchViewModel {
+    var employers: [EmployerModel] = []
+    var isLoading = false
+    var errorMessage: String?
+    
+    func search(filter: String, modelContext: ModelContext) {
+        if filter.count < 2 {
+            loadCachedEmployers(modelContext: modelContext)
+        } else {
+            searchEmployers(filter: filter, modelContext: modelContext)
+        }
+    }
+    
+    private func searchEmployers(filter: String, modelContext: ModelContext) {
+        isLoading = true
+        errorMessage = nil
+        
+        Task {
+            do {
+                let service = NetworkService()
+                try await service.searchEmployers(filter: filter, modelContext: modelContext)
+                
+                // Load from SwiftData after fetch
+                loadCachedEmployers(modelContext: modelContext)
+            } catch {
+                DispatchQueue.main.async {
+                    self.errorMessage = error.localizedDescription
+                }
+            }
+            
+            DispatchQueue.main.async {
+                self.isLoading = false
+            }
+        }
+    }
+
+    private func loadCachedEmployers(modelContext: ModelContext) {
+        // TODO
+    }
+}
